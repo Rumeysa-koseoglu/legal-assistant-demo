8052b2e1d6141fa429c2ebed7b2da570705e2095
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-12-26 20:46:47 +0300

    feat: connect frontend to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import { legalDocuments } from "./data/legalDocs.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 2000;
 
+app.use(cors());
+
 app.get("/api/documents", (req, res) => {
-  res.send(legalDocuments);
+  const keyword = String(req.query.q || "").toLowerCase();
+
+  const result = legalDocuments.filter(
+    (doc) =>
+      doc.title.toLowerCase().includes(keyword) ||
+      doc.content.toLowerCase().includes(keyword)
+  );
+  res.json(result);
 });
 
 app.use((err: Error, req: any, res: any, next: any) => {
--- frontend/src/components/LegalSearchPanel.tsx
+++ frontend/src/components/LegalSearchPanel.tsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LegalSearchPanel() {
+  type Doc = {
+    id: string;
+    title: string;
+    content: string;
+  };
+
+  const [query, setQuery] = useState<string>("");
+  const [docs, setDocs] = useState<Doc[]>([]);
+
+  const URL = "http://localhost:2000/api/documents";
+
+  const filteredSearch = async () => {
+    const response = await fetch(`${URL}?q=${query}`);
+    const data = await response.json();
+    console.log(data);
+    setDocs(data);
+  };
+
+  // const [inputText, setInputText] = useState<string>("");
+
+  //FIRST SOLUTION--------
+  // const [docs, setDocs] = useState<Doc[]>([]);
+
+  //
+  // const search = async () => {
+  //   const res = await fetch(`${URL}?q=${query}`);
+  //   const data = await res.json();
+  //   setDocs(data);
+  // };  FIRST SOLUTION-------
+
+  //
+
+  // const handleRequest = async () => {
+  //   const response = await fetch(URL);
+  //   if (!response.ok) {
+  //     const errorText = await response.text();
+  //     throw new Error(errorText);
+  //   }
+  //   const data = await response.json();
+  //   console.log(data);
+  // };
+
   return (
     <div>
       <div className="panel-container">
-        <input type="text" />
-        <button>search</button>
-        <div className="document-section"></div>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search legal docs.."
+        />
+        <button onClick={filteredSearch}>search</button>
+        <div className="document-section">
+          {docs.map((doc) => (
+            <div key={doc.id}>
+              <h3>{doc.title}</h3>
+              <p>{doc.content}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );

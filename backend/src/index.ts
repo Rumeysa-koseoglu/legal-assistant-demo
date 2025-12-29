0df31f14d2322022b02becc098941e7cc3360480
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-12-29 12:23:50 +0300

    feat(backend): implement gemini ai summarization logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,20 @@
 import express from "express";
 import { legalDocuments } from "./data/legalDocs.js";
 import cors from "cors";
+import "dotenv/config";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express();
 const PORT = 2000;
-
+const apiKey = process.env.GEMINI_API_KEY || "";
+app.use(express.json());
 app.use(cors());
+
+const genAI = new GoogleGenerativeAI(apiKey);
 
 app.get("/api/documents", (req, res) => {
   const keyword = String(req.query.q || "").toLowerCase();
-
-  app.post("/api/summarize", async (req, res) => {
-    const { content } = req.body;
-    res.json({ summary: "this is a temporary mock summary." });
-  });
 
   const result = legalDocuments.filter(
     (doc) =>
@@ -21,6 +22,17 @@ app.get("/api/documents", (req, res) => {
       doc.content.toLowerCase().includes(keyword)
   );
   res.json(result);
+});
+
+app.post("/api/summarize", async (req, res) => {
+  const content = req.body;
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
+  const prompt = `You are a professional legal assistant. Summarize the following legal document into 3 clear bullet points for a non-lawyer. Focus on obligations and risks
+
+    Document: ${content}`;
+
+  const result = await model.generateContent(prompt);
+  res.json({ summary: result.response.text() });
 });
 
 app.use((err: Error, req: any, res: any, next: any) => {

facb1d7975f0718414072d4a1ff02945b33167e8
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-01-01 10:35:58 +0300

    feat(frontend): add "All" to the categories

--- frontend/src/components/Categories.tsx
+++ frontend/src/components/Categories.tsx
@@ -2,13 +2,16 @@ function Categories() {
   return (
     <div className="flex flex-row items-center gap-3">
       <span className=" text-[#845026] font-semibold">Categories :</span>
-      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway">
+      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway cursor-pointer">
+        All
+      </button>
+      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway cursor-pointer">
         Contract
       </button>
-      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway">
+      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway cursor-pointer">
         Agreement
       </button>
-      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway">
+      <button className="bg-[#FCE2CE] px-4 py-1 rounded-full text-[#92613A] font-raleway cursor-pointer">
         Policy
       </button>
     </div>

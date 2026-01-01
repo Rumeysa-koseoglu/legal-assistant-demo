520e77bfe3556023fa491015e285a1bf39f0dbde
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-01-01 16:40:53 +0300

    feat(frontend): add ErrorMessage component

---
+++ frontend/src/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+function ErrorMessage() {
+  return (
+    <div
+      className="p-4 mb-4 text-sm text-fg-danger-strong rounded-base bg-danger-soft"
+      role="alert"
+    >
+      <span>Please type something to search</span>
+    </div>
+  );
+}
+
+export default ErrorMessage;

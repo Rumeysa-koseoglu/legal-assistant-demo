7a63726d49561095db92767ae145665f82085aeb
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-01-01 16:53:20 +0300

    feat(frontend): add styling to ErrorMessage component

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -1,9 +1,12 @@
+import { PiSealWarning } from "react-icons/pi";
+
 function ErrorMessage() {
   return (
     <div
-      className="p-4 mb-4 text-sm text-fg-danger-strong rounded-base bg-danger-soft"
+      className="flex flex-row gap-4 items-center px-2 py-3 w-100 text-sm font-raleway text-red-900 rounded relative z-1 bg-red-200 border-l-3 border-red-400"
       role="alert"
     >
+      <PiSealWarning className="size-6" />
       <span>Please type something to search</span>
     </div>
   );
